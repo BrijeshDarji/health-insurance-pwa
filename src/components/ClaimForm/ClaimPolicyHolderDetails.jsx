ef0041885ceb9dc1d0265843094ad25e74dfc452
Brijesh Darji <brijeshdarjiprofessional@gmail.com>
2023-07-11 20:52:27 +0530

    feat: :art: seperated form fields and its display logic

--- src/components/ClaimForm/ClaimPolicyHolderDetails.jsx
+++ src/components/ClaimForm/ClaimPolicyHolderDetails.jsx
@@ -1,46 +1,16 @@
 import React, { memo } from "react";
-import { PhoneGroup } from "./ClaimForm.style.js";
 
-import OutlinedTextInput from "../form_components/OutlinedTextInput";
-import OutlinedSelect from "../form_components/OutlinedSelect.jsx";
+import { POLICY_HOLDER_DETAIL_FIELDS } from "../../helpers/FormFields.js";
+import { getDynamicElements } from "../../helpers/Utils.js";
 
 function ClaimPolicyHolderDetails() {
     return (
         <>
-            {/* Stepper Forms */}
-
-            <OutlinedTextInput
-                label="First Name"
-                name="policyHolderFirstName"
-                placeholder="Enter first name"
-            />
-            <OutlinedTextInput
-                label="Last Name"
-                name="policyHolderLastName"
-                placeholder="Enter last name"
-            />
-            <OutlinedTextInput
-                label="Policy number"
-                name="policyHolderFirstName"
-                placeholder="Enter policy number"
-            />
-            <OutlinedTextInput
-                label="Email Address"
-                name="policyHolderEmail"
-                placeholder="Enter email address"
-            />
-            <PhoneGroup>
-                <OutlinedSelect
-                    label="Phone Number"
-                    name="policyHolderPhoneCode"
-                    placeholder="Enter phone number"
-                />
-                <OutlinedTextInput
-                    name="policyHolderPhoneNumber"
-                    placeholder="Enter phone number"
-                    type="number"
-                />
-            </PhoneGroup>
+            {
+                POLICY_HOLDER_DETAIL_FIELDS.map((field, index) => (
+                    getDynamicElements(field, {})
+                ))
+            }
         </>
     );
 }
