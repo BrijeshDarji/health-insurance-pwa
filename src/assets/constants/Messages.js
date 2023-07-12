f77a12da68cc770577bfb2672f92adac21fd0304
Brijesh Darji <brijeshdarjiprofessional@gmail.com>
2023-07-12 23:53:41 +0530

    feat: :memo: added common system messages file

---
+++ src/assets/constants/Messages.js
@@ -0,0 +1,10 @@
+/**
+ *  @description This file contains all system messages.
+ */
+
+export const ERROR_MESSAGES = {
+    ALL_REQUIRED: "Please make sure all the required fields are filled and are valid.",
+    DOCUMENTS: {
+        ADD_DOCUMENT: "Please add at least one document for the claim."
+    }
+}
