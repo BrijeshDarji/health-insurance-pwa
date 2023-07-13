c92769a5e3a4bd0f9c56f17dfd31629ee284a2d1
mitashupatel <mitashu@openxcellinc.com>
2023-07-13 17:30:40 +0530

    feat(formComponents): :sparkles: changed pabblt API path to original one

--- src/assets/constants/ApiPath.js
+++ src/assets/constants/ApiPath.js
@@ -2,4 +2,4 @@
  *  @description This file contains API routes.
  */
 
-export const FORM_SUBMIT_PATH = "https://connect.pabbly.com/workflow/sendwebhookdata/IjU3NjUwNTZjMDYzNzA0MzU1MjZkNTUzZDUxMzci_pc"
+export const FORM_SUBMIT_PATH = "https://connect.pabbly.com/workflow/sendwebhookdata/IjU3NjUwNTZkMDYzZjA0MzM1MjZjNTUzZDUxMzMi_pc"
